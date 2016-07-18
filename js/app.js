f0af89840e85b51b31da52efb19cb0f361e243ea
Harrisonkamau <kamauharrison87@gmail.com>
2016-07-18 14:38:09 +0300

    create dom shortcuts

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var ref = new Firebase('https://webchat-e0aff.firebaseio.com ');
+function WebChat(){
+  this.checkSetup();
+  // / Shortcuts to DOM Elements.
+  this.messageList = document.getElementById('messages');
+  this.messageForm = document.getElementById('message-form');
+  this.messageInput = document.getElementById('message');
+  this.submitButton = document.getElementById('submit');
+  this.submitImageButton = document.getElementById('submitImage');
+  this.imageForm = document.getElementById('image-form');
+  this.mediaCapture = document.getElementById('mediaCapture');
+  this.userPic = document.getElementById('user-pic');
+  this.userName = document.getElementById('user-name');
+  this.signInButton = document.getElementById('sign-in');
+  this.signOutButton = document.getElementById('sign-out');
+  this.signInSnackbar = document.getElementById('must-signin-snackbar');
+}
