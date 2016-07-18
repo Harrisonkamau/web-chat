d6fbcc524ebfafb9ce1e7408be0547336c719b15
Harrisonkamau <kamauharrison87@gmail.com>
2016-07-18 11:41:52 +0300

    authorize login with google

--- js/app.js
+++ js/app.js
@@ -7,3 +7,9 @@ FriendlyChat.prototype.initFirebase = function() {
   // Initiates Firebase auth and listen to auth state changes.
   this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
+
+FriendlyChat.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
