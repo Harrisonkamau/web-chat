cc25df483f8483b8471711291a06200a01558b1c
Harrisonkamau <kamauharrison87@gmail.com>
2016-07-18 14:40:15 +0300

    initiaize firebase

--- js/app.js
+++ js/app.js
@@ -13,4 +13,43 @@ function WebChat(){
   this.signInButton = document.getElementById('sign-in');
   this.signOutButton = document.getElementById('sign-out');
   this.signInSnackbar = document.getElementById('must-signin-snackbar');
+
+  // Saves message on form submit.
+  this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
+  this.signOutButton.addEventListener('click', this.signOut.bind(this));
+  this.signInButton.addEventListener('click', this.signIn.bind(this));
+
+  // Toggle for the button.
+  var buttonTogglingHandler = this.toggleButton.bind(this);
+  this.messageInput.addEventListener('keyup', buttonTogglingHandler);
+  this.messageInput.addEventListener('change', buttonTogglingHandler);
+
+  // Events for image upload.
+  this.submitImageButton.addEventListener('click', function() {
+    this.mediaCapture.click();
+  }.bind(this));
+  this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+
+  this.initFirebase();
+
 }
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+WebChat.prototype.initFirebase = function() {
+  // TODO(DEVELOPER): Initialize Firebase.
+};
+
+// Loads chat messages history and listens for upcoming ones.
+WebChat.prototype.loadMessages = function() {
+  // TODO(DEVELOPER): Load and listens for new messages.
+};
+
+// Saves a new message on the Firebase DB.
+WebChat.prototype.saveMessage = function(e) {
+  e.preventDefault();
+  // Check that the user entered a message and is signed in.
+  if (this.messageInput.value && this.checkSignedInWithMessage()) {
+
+    // TODO(DEVELOPER): push new message to Firebase.
+
+  }
+};
